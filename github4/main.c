d71c5584c981a1c839f6189e78dead0252aef3b6
Raluca-1 <mraluca464@gmail.com>
2025-05-14 22:09:19 +0300

    Add files via upload

---
+++ github4/main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    int a,b;
+    int suma;
+    printf("Introduceti valoarea lui a:");
+    scanf("%d",&a);
+    printf("\nIntroduceti valoarea lui b:");
+    scanf("%d",&b);
+
+    suma=a+b;
+    printf("\nSuma celor doua numere citite de la tastatura este:%d",suma);
+
+getch();
+
+return 0;
+}
